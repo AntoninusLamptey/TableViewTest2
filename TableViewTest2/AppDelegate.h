4977aaa183d33a17eec4bc90c6c532eb4c067d22
AntoninusLamptey <antoninus@mail.usf.edu>
2017-04-13 12:45:25 -0400

    Initial Commit

---
+++ TableViewTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewTest2
+//
+//  Created by Antoninus Lamptey on 4/13/17.
+//  Copyright © 2017 Antoninus Lamptey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewTest2
+//
+//  Created by Antoninus Lamptey on 4/13/17.
+//  Copyright © 2017 Antoninus Lamptey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
