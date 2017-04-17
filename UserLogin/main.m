4889a4a191378bc920ebf553f8a07204ac7f3403
madhumahat <madhumahat@Madhus-MacBook-Pro.local>
2017-04-16 20:00:41 -0400

    Initial Commit

---
+++ UserLogin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserLogin
+//
+//  Created by Madhu Mahat on 4/16/17.
+//  Copyright © 2017 Madhu Mahat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
